2a9209df283f1862dfd908581e84705c40bdc605
unknown <jakab.attila2001@gmail.com>
2022-03-17 11:54:55 +0100

    kesz

---
+++ src/app/mock-vehicles.ts
@@ -0,0 +1,10 @@
+import {Vehicle} from "./vehicle";
+
+export const VEHICLES: Vehicle[] =[
+  {id:1, plate: "DGA-574" , brand: "Nissan" , price: 870000},
+  {id:2, plate: "ASD-323" , brand: "Ford" , price: 750000},
+  {id:3, plate: "HDG-654" , brand: "BMW" , price: 670000},
+  {id:4, plate: "NSE-421" , brand: "Honda" , price: 570000},
+  {id:5, plate: "GFD-654" , brand: "Fiat" , price: 620000},
+  {id:6, plate: "HEW-576" , brand: "Audi" , price: 560000},
+]
